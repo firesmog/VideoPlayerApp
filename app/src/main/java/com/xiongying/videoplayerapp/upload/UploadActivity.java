6fab7da353cf8e717be961547b18561313dada01
nickming <962570483@qq.com>
2017-03-20 09:11:35 +0800

    划分了模块

---
+++ app/src/main/java/com/xiongying/videoplayerapp/upload/UploadActivity.java
@@ -0,0 +1,15 @@
+package com.xiongying.videoplayerapp.upload;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xiongying.videoplayerapp.R;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
